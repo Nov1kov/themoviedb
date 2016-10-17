a0288e6582c14d2b96182a316579a311aca50659
Nov1kov <spellh1@gmail.com>
2016-10-17 04:37:40 +0300

    * rename entity

---
+++ app/src/main/java/ru/novikov/themoviedb/model/entity/BelongsToCollection.java
@@ -0,0 +1,14 @@
+package ru.novikov.themoviedb.model.entity;
+
+/**
+ * Created by Ivan on 09.10.2016.
+ */
+
+public class BelongsToCollection {
+
+    public Integer id;
+    public String name;
+    public String posterPath;
+    public String backdropPath;
+
+}
---
+++ app/src/main/java/ru/novikov/themoviedb/model/entity/Genre.java
@@ -0,0 +1,12 @@
+package ru.novikov.themoviedb.model.entity;
+
+/**
+ * Created by Ivan on 09.10.2016.
+ */
+
+public class Genre {
+
+    public Integer id;
+    public String name;
+
+}
